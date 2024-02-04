2c2e2ee4f52cce15cbcebc5e7e5a4a176a9d17b1
Hafiz Ahmed Ali Ansari <117213903+ahmedaliansari@users.noreply.github.com>
2024-02-04 11:48:42 +0500

    Java Script Practice
    * alert
    * prompt
    * Var, Let, Const Variable
    * Data Types
    * Loops
    
    Ye Sare Chize Mene Aj Sikhi Ha, Happy Coding 😍...

---
+++ Java Script/Class No 1/app.js
@@ -0,0 +1,71 @@
+// let name = "ahmed";
+
+// console.log(name);
+// // alert(name);
+
+// var uname = prompt("What Is Your Name");
+// console.log(uname);
+
+// * Deifference Between Var, Let, Const Variable
+// var Work Global Varriable;
+// let Work Local Varriable;
+// Const Work Fixed Varriable; But Not Inatialized & Update Values
+
+// Example Simple User Details In Prompt
+
+// let name = prompt("What Is Your Name");
+// let job = prompt("What Is Your Job");
+// let gender = prompt("What Is Your Gender");
+// let age = prompt("What Is Your Age");
+// let experience = prompt("What Is Your Experience");
+
+// let result = `
+// My Name Is ${name}<br>
+// My Job Is ${job}<br>
+// My Gender Is ${gender}<br>
+// My Age Is ${age}<br>
+// My Experience Is ${experience}
+// `;
+
+// document.write(result);
+
+// Java Script 2 Data Types;
+
+// 1) Premitive
+// 2) Refrence
+
+// * Premitive
+// 1) String
+// 2) Number
+// 3) Boolean
+// 4) Null
+// 5) Undefined
+
+// * Refrence
+
+// LOOPS
+// 6 Types Of Loops In Java Script
+
+// (*) For (*) While (*) Do While (*) For In (*) For Of (*) For Each Method
+
+// (*) For Loop: ~
+// let a = 1;
+
+// for (; a <= 10; a++) {
+//   console.log(a);
+// }
+
+// (*) While LOOP:~
+// let a = 1;
+// while (a <= 10) {
+//   console.log(a);
+//   a++;
+// }
+
+// (*) Do While LOOP:~
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
