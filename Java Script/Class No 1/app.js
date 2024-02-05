c4826d965fccabc63ecff6cab20c006cd417413b
Hafiz Ahmed Ali Ansari <117213903+ahmedaliansari@users.noreply.github.com>
2024-02-05 15:10:09 +0500

    Java Script Class No 1 Practice:~
    Here Im Solve Some Question :~
    
    * Simple Calculator Function
    * Default Parmeter Function
    * Switch Cases
    * Input Values Get Function & Append
      Text Default And Prompt Function...

--- Java Script/Class No 1/app.js
+++ Java Script/Class No 1/app.js
@@ -1,3 +1,5 @@
+// 04 - 02 - 2024 Sunday
+
 // let name = "ahmed";
 
 // console.log(name);
@@ -69,3 +71,86 @@
 //   i++;
 // } while (i <= 10);
 
+// Simple Calculator Function
+
+// function add(num1, operator, num2) {
+//   num1 = parseFloat(num1);
+//   num2 = parseFloat(num2);
+
+//   if (isNaN(num1) || isNaN(num2)) {
+//     return "Invalid input. Please enter valid numbers.";
+//   }
+
+//   if (operator === "+") {
+//     return num1 + num2;
+//   } else if (operator === "-") {
+//     return num1 - num2;
+//   } else if (operator === "*") {
+//     return num1 * num2;
+//   } else if (operator === "/") {
+//     return num1 / num2;
+//   } else {
+//     return "Undefined Operator";
+//   }
+// }
+
+// var first = prompt(`Your First Number`);
+// var second = prompt(`Your Second Number`);
+// var operator = prompt(`Choose Your Operator`);
+
+// var result = add(first, operator, second);
+// console.log(result);
+
+// Local & Global Varriavbles:~
+
+// var a = "Hello World"; // Declare Global Variable
+
+// function check() {
+//   var a = "Hello"; // Update Global Variable To Local Function
+//   console.log(a);
+// }
+
+// check();
+// console.log(a); // Call Global Variable
+
+// 05 - 02 - 2024 Monday
+
+// Default Paramenter Function:~
+
+// function add(num1, num2 = 50) { // Set Default Value num2 = 50 //
+//   return num1 + num2;
+// }
+
+// let result = add(5, 10);
+// console.log(result);
+
+// Simple Swith Case
+
+// var Name = prompt("Enter You Name");
+
+// switch (Name) {
+
+//     case "Ahmed": alert("Hello Ahmed"); break;
+//     case "Sajjad": alert("Hello Sajjad"); break;
+//     case "Asharif": alert("Hello Asharif"); break;
+//     case "Taha": alert("Hello Taha"); break;
+//     case "Zunaira": alert("Hello Zunaira"); break;
+
+//     default : alert("Hello");
+
+// }
+
+// Input Values Get Function:~
+
+// function user() {
+//   var name = document.getElementById("username");
+//   console.log(name.value);
+//   name.value = "";
+// }
+
+// Insert Paragraph Using JavaScript Default Text & Prompt:~
+
+// var para = document.getElementById("para");
+// var text = prompt('Type Your Text Here');
+
+// para.innerHTML += text;
